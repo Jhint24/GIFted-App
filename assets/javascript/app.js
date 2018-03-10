6843f5ee4d24b093e8adc923eb13c2f625189826
Josh <jhint2424@gmail.com>
2018-03-09 21:36:18 -0700

    continued script

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,5 @@
 //create variables 
-var apiKey = "9a5h6VPUl9kDUeWtRsK0jgVqS9PUJNi3";
-var searchTerm;
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + searchTerm + "&limit=10"; //ratinglang = &rating=G&lang=en
+ //ratinglang = &rating=G&lang=en
 //create an array of strings as var = topic 
 var topics = ["Fortnite", "Rocket League", "Brawlhalla", "Call of Duty", "Destiny", "Overwatch"];
 //take the topics in the array and create buttons in HTML
@@ -20,9 +18,18 @@ var topics = ["Fortnite", "Rocket League", "Brawlhalla", "Call of Duty", "Destin
         $("#topics-view").append(a);
     }
 }
-renderButtons();
+
+$("button").on("click", function()
+{
+    var videoGames = $(this).attr("data-name");
+    var apiKey = "9a5h6VPUl9kDUeWtRsK0jgVqS9PUJNi3";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + videoGames + "&limit=10";
+    
+
+})
 //make sure the gifs are non-animated when grabbed/appended
 //when user clicks image, animate
 //display the rating
 //add a form to take values from user to add into topics array
 //make a function that takes each topc and remakes the buttons on page
+renderButtons();
